e1f90a7e247a444b51b7c2dedf7423e56a8ee3ca
Martin Christen <martin.christen@fhnw.ch>
2019-07-14 08:30:50 +0200

    added wprking example downloading tiles

---
+++ flask-swisstopo/start.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask import render_template
+import requests
+import uuid
+
+
+app = Flask(__name__)
+
+# http://wmts.geo.admin.ch/1.0.0/WMTSCapabilities.xml
+# <ows:LowerCorner>5.140242 45.398181</ows:LowerCorner>
+# <ows:UpperCorner>11.47757 48.230651</ows:UpperCorner>
+
+@app.route('/')
+def hello_world():
+
+    zoom = 19
+    tilex = 30
+    tiley = 49
+
+    url = f"https://wmts6.geo.admin.ch/1.0.0/ch.swisstopo.swissimage/default/current/21781/{zoom}/{tilex}/{tiley}.jpeg"
+
+    payload = ""
+    headers = {
+        'Origin': "https://www.uvek-gis.admin.ch",
+        'cache-control': "no-cache",
+    }
+
+    response = requests.request("GET", url, data=payload, headers=headers)
+
+    filename = f"temp/{zoom}-{x}-{y}.jpg"
+
+    with open(filename, "wb") as image:
+        image.write(response.content)
+
+    return render_template("index.html", user_image=filename)
+
+
+
+
+app.run(debug=True, port=5555)
